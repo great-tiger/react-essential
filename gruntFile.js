50150ec410f0e94723934509cc0aeb7a4651fbaa
great-tiger <892873387@qq.com>
2016-02-15 15:28:50 +0800

    配置grunt-connect静态服务器

---
+++ gruntFile.js
@@ -0,0 +1,14 @@
+﻿module.exports = function(grunt) {
+
+    grunt.initConfig({
+      connect: {
+        server: {
+          port: 9002,
+          base: './'
+        }
+      }
+    });
+    
+    grunt.loadNpmTasks('grunt-connect');
+    grunt.registerTask('default', ['connect:server']);
+};
